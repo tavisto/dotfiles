756f1063ce6047d612cb9d76564b388311c3c473
Tavis Aitken <taitken@taitken-ws.nar.beatportcorp.net>
2009-06-12 18:45:00 -0600

    Removed specific stuff that shouldn't be there

--- bin/updateTags.sh
+++ bin/updateTags.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
 # $HOME/bin/php-ctags.sh %%SOURCE%% %%TAG_NAME%%
-
-$HOME/bin/php-ctags.sh $HOME/source_code/beatport/trunk/server/src beatport_trunk 
-#$HOME/bin/php-ctags.sh $HOME/source_code/beatport/trunk_clean beatport_trunk_clean 
-$HOME/bin/php-ctags.sh $HOME/source_code/beatport/4.1.1/server/src beatport_release 
-$HOME/bin/php-ctags.sh $HOME/source_code/baseware/trunk/server/src baseware_trunk 
-$HOME/bin/php-ctags.sh $HOME/source_code/admintools/trunk/server/src adminTools_trunk 
-$HOME/bin/php-ctags.sh $HOME/source_code/new-admin/trunk/server/src at2_trunk 
-$HOME/bin/php-ctags.sh $HOME/source_code/new-admin/trunk/server/src at2_trunk 
-#$HOME/bin/php-ctags.sh $HOME/source_code/utility/trunk utility
