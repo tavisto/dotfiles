a22a9d1ca283e39a6ce1908917305a66fdb40053
Tavis Aitken <taitken@taitken-ws.nar.beatportcorp.net>
2009-10-26 14:37:28 -0600

    Finished my phpunit auto completion script.

--- .bash_completion.d/phpunitrun.sh
+++ .bash_completion.d/phpunitrun.sh
@@ -1,28 +1,35 @@
 _phpunitrun()
 {
-	local cur prev opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="--log-json --log-tap --log-xml--coverage-html --coverage-clover --coverage-source--test-db-dsn --test-db-log-rev --test-db-prefix --test-db-log-info--story-html --story-text--testdox-html --testdox-text--filter --group --exclude-group --list-groups--loader --repeat--story --tap --testdox--colors --no-syntax-check --stop-on-failure --verbose --wait--skeleton-class --skeleton-test--process-isolation --no-globals-backup --no-static-backup --bootstrap --configuration --no-configuration --include-path -d --help --version "
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--log-junit --log-tap --log-json --coverage-html --coverage-clover --coverage-source --story-html --story-text --testdox-html --testdox-text --filter --group --exclude-group --list-groups --loader --story --tap --testdox --colors --stderr --stop-on-failure --verbose --wait --skeleton-class --skeleton-test --process-isolation --no-globals-backup --no-static-backup --syntax-check --bootstrap --configuration --no-configuration --include-path -d --help --version"
 
-	if [[ ${cur} == -* ]]; then
-		COMPREPLY=( $(compgen -W "${opts}" -- ${cur} ) )
-		return 0
-	fi
+    if [[ ${cur} == -* ]]; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur} ) )
+        return 0
+    fi
 
-	case "${prev}" in
-		 --help)
-			return 0
-			;;
-		 --version)
-		 	return 0
-			;;
-		*)
-			COMPREPLY=( $(compgen -f ${cur}) )
-			return 0
-			;;
-	esac
+    case "${prev}" in
+        --@(help|version))
+            return 0
+            ;;
+        # Directory completion options
+        --@(coverage-html|coverage-source))
+            _filedir -d
+            return 0;
+            ;;
+        --@(group|exclude-group))
+            GROUP=$(fgrep -ih '@group' *.php | sed 's/@group //' | tr '\n' ' ') 
+            COMPREPLY=( $(compgen -W "${GROUP}" ${cur} ) )
+            return 0;
+            ;;
+        *)
+        COMPREPLY=( $(compgen -f ${cur}) )
+        return 0
+        ;;
+    esac
 
 }
 complete -F _phpunitrun phpunitrun
