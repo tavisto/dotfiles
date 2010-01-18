4266b1d4da82f687f8cd7a7f8e3f0cc5741150d0
Tavis Aitken <tavisto@gmail.com>
2010-01-18 11:48:24 -0700

    Removed beatport specific stuff

--- bin/updateTags.sh
+++ bin/updateTags.sh
@@ -4,21 +4,6 @@
 
 echo "Updating $1"
 case $1 in
-beatport)
-    $HOME/bin/php-ctags.sh $SOURCE_CODE_DIR/beatport/trunk/php beatport_trunk 
-    ;;
-mobile)
-    $HOME/bin/php-ctags.sh $SOURCE_CODE_DIR/mobile/trunk/php mobile
-    ;;
-catalog)
-    $HOME/bin/php-ctags.sh $SOURCE_CODE_DIR/api/catalog/trunk catalog
-    ;;
-admin)
-    $HOME/bin/php-ctags.sh $SOURCE_CODE_DIR/admintools/trunk/server/src at
-    ;;
-control)
-    $HOME/bin/php-ctags.sh $SOURCE_CODE_DIR/control/trunk/server/src control 
-    ;;
 zf)
     $HOME/bin/php-ctags.sh /usr/share/php/Zend zf 
     ;;
