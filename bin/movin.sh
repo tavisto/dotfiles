e363e7d5d4964b941276ebbbcb886b1219d4eb5a
Tavis Aitken <tavisto@gmail.com>
2010-01-17 21:33:14 -0700

    *) Fixed the movin script to copy the .hgrc file as well.

--- bin/movin.sh
+++ bin/movin.sh
@@ -3,7 +3,7 @@
 CONFIG_DIR="$HOME/.home-config"
 
 # Don't copy the repo directory or the . and .. files.
-DOTFILES=`ls -a $CONFIG_DIR| grep -v '\.\.' | grep -v '\.$'| grep -v '\.hg'` 
+DOTFILES=`ls -aI .hg  $CONFIG_DIR| grep -v '\.\.' | grep -v '\.$'`
 for i in $DOTFILES
 do
     if [ $1 == 'clean' ]; then
