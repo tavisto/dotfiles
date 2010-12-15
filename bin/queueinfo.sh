c1ad7acc3c4acd0ec1479572b608efad39a6ba7c
Tavis Aitken <devnull@localhost>
2010-12-15 09:27:57 -0700

    Updated .hgrc

--- bin/queueinfo.sh
+++ bin/queueinfo.sh
@@ -8,19 +8,19 @@ echo "Connection Info: "
 echo "vhost user IP state channels timeout client_properties" | column -t
 echo "------------------------------------------------------------------"
 sudo rabbitmqctl -q list_connections vhost user address state channels timeout client_properties | column -t 
-#echo "------------------------------------------------------------------"
-#echo "Channels:"
-#echo "number user vhost transactional consumer_count messages_unacknowledged prefetch_count" | column -t
-#echo "------------------------------------------------------------------"
-#sudo rabbitmqctl -q list_channels number user vhost transactional consumer_count messages_unacknowledged prefetch_count
-#echo "------------------------------------------------------------------"
-#echo "Consumers:"
-#echo " queue connection_pid consume_tag acks_expected" | column -t
-#echo "------------------------------------------------------------------"
-#sudo rabbitmqctl -q list_consumers -p /$1 | column -t 
-#echo "------------------------------------------------------------------"
-#echo "Exchange Info: "
-#echo " name type durable auto_delete"
-#echo "------------------------------------------------------------------"
-#sudo rabbitmqctl -q list_exchanges -p /$1 name type durable auto_delete | column -t
-#echo "------------------------------------------------------------------"
+echo "------------------------------------------------------------------"
+echo "Channels:"
+echo "number user vhost transactional consumer_count messages_unacknowledged prefetch_count" | column -t
+echo "------------------------------------------------------------------"
+sudo rabbitmqctl -q list_channels number user vhost transactional consumer_count messages_unacknowledged prefetch_count
+echo "------------------------------------------------------------------"
+echo "Consumers:"
+echo " queue connection_pid consume_tag acks_expected" | column -t
+echo "------------------------------------------------------------------"
+sudo rabbitmqctl -q list_consumers -p /$1 | column -t 
+echo "------------------------------------------------------------------"
+echo "Exchange Info: "
+echo " name type durable auto_delete"
+echo "------------------------------------------------------------------"
+sudo rabbitmqctl -q list_exchanges -p /$1 name type durable auto_delete | column -t
+echo "------------------------------------------------------------------"
