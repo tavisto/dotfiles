5a054e5c57d209bed49d38d09888e3d4ffb5e050
Tavis Aitken <tavis.aitken@beatport.com>
2012-02-02 10:51:56 -0700

    Made the port for autossh be set in the .bash_local to seperate environments

--- bin/autossh.sh
+++ bin/autossh.sh
@@ -6,11 +6,11 @@
 #fi
  
 AUTOSSH_POLL=600
-AUTOSSH_PORT=20010
+#AUTOSSH_PORT=20020 Set this in .bash_local to avoid colisions 
 AUTOSSH_GATETIME=30
 AUTOSSH_LOGFILE=/tmp/autossh.log
 AUTOSSH_DEBUG=yes 
 AUTOSSH_PATH=/usr/bin/ssh
 export AUTOSSH_POLL AUTOSSH_LOGFILE AUTOSSH_DEBUG AUTOSSH_PATH AUTOSSH_GATETIME AUTOSSH_PORT
  
-autossh -2 -fN -M 20010 -v -D 1080 tavisto@node01.tavisto.net
+autossh -2 -fN -M $AUTOSSH_PORT -v -D 1080 tavisto@node01.tavisto.net
