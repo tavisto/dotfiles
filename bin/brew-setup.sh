cf396d22174ba759c37476e82603d1716d68f4be
Tavis Aitken <tavisto@tavisto.net>
2014-01-17 21:16:59 -0500

    Fixed the brew install script.

--- bin/brew-setup.sh
+++ bin/brew-setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-BREW='brew install '
-
-packages='macvim vim markdown ctags wget tree bash bash-completion2'
-for package in $packages
-do
-    $BREW $package
-done
+BREW='/usr/local/bin/brew'
+packages='rock-cli git macvim vim markdown ctags wget tree bash bash-completion2'
+if [ -f $BREW ]; then
+    $BREW install $packages
+    $BREW linkapps
+fi
