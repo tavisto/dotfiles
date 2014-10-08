238647fef9698a7d0adfd0edac67eacf690d98ec
Tavis Aitken <taitken@shutterstock.com>
2014-10-08 13:29:05 -0600

    Updated brew list as well as updated the bash fish script.

--- bin/bash-fix.sh
+++ bin/bash-fix.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 bash -c "f() { x() { _;}; x() { _;} <<a; }" 2>/dev/null || echo vulnerable
-
+brew install bash || brew upgrade bash
 sudo sh -c 'echo "/usr/local/bin/bash" >> /etc/shells'
 chsh -s /usr/local/bin/bash
-sudo mv /bin/bash /bin/bash-backup
 sudo ln -s /usr/local/bin/bash /bin/bash
